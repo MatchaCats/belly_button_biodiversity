b73da23b39b8f71ca1da0299eb9d950df9120a16
MatchaCats <natscholarships18@gmail.com>
2024-11-17 15:53:48 -0800

    Updated metadata panel

--- static/js/app.js
+++ static/js/app.js
@@ -3,22 +3,27 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
+    let result = metadata.filter(sampleObj => sampleObj.id == sample);
+    let sampleMetadata = result[0]; // Assuming there is only one match
 
+    // Use d3 to select the panel with id of #sample-metadata
+    let panel = d3.select("#sample-metadata");
 
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-
-    // Use `.html("") to clear any existing metadata
-
+    // Use .html("") to clear any existing metadata
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    Object.entries(sampleMetadata).forEach(([key, value]) => {
+      panel.append("p").text(`${key}: ${value}`);
+    });
 
   });
 }
+
 
 // function to build both charts
 function buildCharts(sample) {
